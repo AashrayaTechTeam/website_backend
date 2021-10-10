062071189d179fc84d7fabd6a3b22a6b1cdc2c8b
Himangshu1086 <baishyahimangshu499@gmail.com>
2021-10-10 23:35:49 +0530

    start the server

---
+++ __DATABASE/SERVER/initialiseDB.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const DB = process.env.MONGO_URI;
+
+mongoose.connect( DB, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log('connected to mongo');
+}).catch((err)=>{
+    console.log('error connecting' ,err)
+})
+
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const express = require('express');
+const express = require('express')
 const app = express();
-const port = 3000;
+const dotenv = require("dotenv")
+dotenv.config({path:"./config.env"});
+const PORT = process.env.PORT || 5000
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+require("./__DATABASE/SERVER/initialiseDB");
 
-app.get('/', (req, res) => {
-  res.send('Team Aasharaya')
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-});
+// link the router files 
+app.use(require('./__ROUTES/FUNDRAISE/Fund_raise'))
+// app.use(require('./__ROUTER/Auth'))
+// app.use(require('./__ROUTER/Services'))
+// app.use(require('./__ROUTER/Address'))
+
+// if(process.env.NODE_ENV == "production"){
+//     app.use(express.static("frontend/build"));
+
+//     app.get('/*', function(req, res) {
+//         res.sendFile(path.join(__dirname, 'frontend/build/index.html'), function(err) {
+//           if (err) {
+//             res.status(500).send(err)
+//           }
+//         })
+//       })
+// 
+//}
+
+
+
+app.listen( PORT , ()=>{
+    console.log(`server connected IN PORT ${PORT}`)
+})
+
+
+
