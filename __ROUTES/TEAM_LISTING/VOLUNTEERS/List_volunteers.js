b12f6918cfb791c9db9ac98d79647334d7cb82e0
Jayanthee365 <jayanthi.naik@gupshup.io>
2022-02-20 19:56:08 +0530

    fix

--- __ROUTES/TEAM_LISTING/VOLUNTEERS/List_volunteers.js
+++ __ROUTES/TEAM_LISTING/VOLUNTEERS/List_volunteers.js
@@ -1,6 +1,19 @@
-const express = require('express')
+const express = require("express");
+const STATE_REPRESENTATIVE = require("../STATE_REPRESENTATIVE/representative_schema");
 const router = express.Router();
 
+router.get("/volunteers/:tag", async (req, res) => {
+  STATE_REPRESENTATIVE.find({ tag: req.params.tag }, function (error, response) {
+    if (error) {
+      console.log(error);
+      res.status(400).send({
+        status: "error",
+        message: "Error while fetching data to db",
+      });
+    } else {
+      res.send(response);
+    }
+  });
+});
 
-
-module.exports = router
+module.exports = router;
